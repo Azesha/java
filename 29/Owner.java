e6eca0d219e7cc36e9a58b892a310248f4f02233
Azesha <azeshatripathy4@gmail.com>
2021-10-29 21:39:06 -0700

    adding 29 assg files

---
+++ 29/Owner.java
@@ -0,0 +1,14 @@
+public class Owner{
+	public Owner(int a) {
+		System.out.println("When you are going for marketing servent");
+	}
+	
+	public Owner(String s) {
+		System.out.println("what you purchased servent ?");
+		
+	}
+	
+	public Owner(double d) {
+		System.out.println("how much remained ?");
+	}
+}
