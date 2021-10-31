11ca1c0af200be8bb189ab5aa0e0a333bba9c1a7
Azesha <67699888+Azesha@users.noreply.github.com>
2021-10-31 00:04:56 -0700

    Add files via upload

---
+++ FirstFriend.java
@@ -0,0 +1,20 @@
+
+
+public class FirstFriend {
+	public FirstFriend() {
+		System.out.println("Please give me Azesha's number");
+		
+	}
+	FirstFriend(int a){
+		System.out.println("wrong number");
+	}
+	
+	
+	
+
+	     
+	
+	
+
+
+}
